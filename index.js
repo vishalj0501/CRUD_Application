3c7eadb318db79f1a1cd825033f0c532a154e86c
vishalj0501 <jvishal0501@gmail.com>
2022-09-24 00:37:59 +0400

    Add files

---
+++ index.js
@@ -0,0 +1,29 @@
+require('./models/db');
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+const employeeController = require('./controller/employeecontrol');
+
+var app = express();
+
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+
+app.use(bodyparser.json());
+
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+app.listen(3000, () => {
+    console.log('Port Started');
+});
+
+app.use('/employee', employeeController);
